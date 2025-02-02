03bf3f8c163bb5234945b1d3f12cc5b82da1812a
Andrew Closs <andrewcloss@gmail.com>
2025-02-02 02:08:16 -0500

    Add fzf, catbat

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -9,31 +9,38 @@ alias l=ls
 alias g=git
 
 # git
-alias ga=git add .
-alias gs=git status
-alias gc=git checkout
-alias gcb=git checkout -b
-alias gca=git commit --amend
+alias ga='git add .'
+alias gs='git status'
+alias gd='git diff'
+alias gp='git push'
+alias gc='git checkout'
+alias gcb='git checkout -b'
+alias gca='git commit --amend --no-edit'
 
-# mask built-ins with better defaults
-alias vi=vim
-
-# more ways to ls
-alias ls="exa -l --color=always --icons --group-directories-first"
-alias la="exa -lah --color=always --icons --group-directories-first"
+# more ways to ls (mask)
+alias ls='exa -l --color=always --icons --group-directories-first'
+alias la='exa -lah --color=always --icons --group-directories-first'
 alias ldot='exa -ld --color=always --icons --group-directories-first .*'
+
+# cat (mask)
+alias bat=batcat
+
+# find
+alias fzf='fzf --preview "batcat --color=always {}"'
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+
+# tar
+alias tarls='tar -tvf'
+alias untar='tar -xf'
+
+# vi (mask)
+alias vi=vim
 
 # fix common typos
 alias quit='exit'
 alias cd..='cd ..'
 
-# tar
-alias tarls="tar -tvf"
-alias untar="tar -xf"
-
-# find
-alias fd='find . -type d -name'
-alias ff='find . -type f -name'
 
 # url encode/decode
 alias urldecode='python3 -c "import sys, urllib.parse as ul; \
